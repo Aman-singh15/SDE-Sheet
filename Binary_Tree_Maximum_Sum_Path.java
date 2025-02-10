f012db1f775a6c979c30a478fffe77ac2bc8a181
Aman-singh15 <aman15dece@gmail.com>
2025-02-10 23:44:22 +0530

    Create Binary_Tree_Maximum_Sum_Path.java

---
+++ Binary_Tree_Maximum_Sum_Path.java
@@ -0,0 +1,51 @@
+/*A path in a binary tree is a sequence of nodes where each pair of adjacent nodes in the sequence has an edge connecting them. A node can only appear in the sequence at most once. Note that the path does not need to pass through the root.
+The path sum of a path is the sum of the node's values in the path.
+Given the root of a binary tree, return the maximum path sum of any non-empty path.
+Example 1:
+Input: root = [1,2,3]
+Output: 6
+Explanation: The optimal path is 2 -> 1 -> 3 with a path sum of 2 + 1 + 3 = 6.
+Example 2:
+Input: root = [-10,9,20,null,null,15,7]
+Output: 42
+Explanation: The optimal path is 15 -> 20 -> 7 with a path sum of 15 + 20 + 7 = 42.
+
+
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int maxPathSum(TreeNode root) {
+        int[] maxi = {Integer.MIN_VALUE};
+        findMaxPathSum(root, maxi);
+        return maxi[0];
+    }
+    public int findMaxPathSum(TreeNode root, int[] maxi) {
+        if (root == null) {
+            return 0;
+        }
+        // Calculate the maximum path sum
+        // for the left and right subtrees
+        int leftMaxPath = Math.max(0, findMaxPathSum(root.left, maxi));
+        int rightMaxPath = Math.max(0, findMaxPathSum(root.right, maxi));
+        // Update the overall maximum
+        // path sum including the current node
+        maxi[0] = Math.max(maxi[0], leftMaxPath + rightMaxPath + root.val);
+        // Return the maximum sum considering
+        // only one branch (either left or right)
+        // along with the current node
+        return Math.max(leftMaxPath, rightMaxPath) + root.val;
+    }
+
+}
